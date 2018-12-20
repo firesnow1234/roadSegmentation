224226fca614c101986d57b39e8c01587be9011e
firesnow1234 <18868113090@163.com>
2018-12-20 10:25:39 +0800

    Update main.cc

--- linux_C/main.cc
+++ linux_C/main.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <math.h>
 #include <Eigen/Eigen>
 #include <pcl/ModelCoefficients.h>
 #include <pcl/sample_consensus/method_types.h>
@@ -18,7 +19,7 @@ using namespace std;
 
 const double Pi =  3.141592;
 
-
+// test success !!!
 void compute_hori_angle(MatrixXd &velo, vector<double> &angle_v, vector<double> &angle_h, vector<int> &dangle_h, vector<int> &index){
     int ind_j = 0;
     double lastveh = 0;
@@ -80,7 +81,7 @@ int main()
 
     // 激光雷达数据 用MatrixXd存储
     long long lidarLens = cloud->points.size();
-    const int width = 3;
+    int width = 3;
     MatrixXd matrix_lidar = MatrixXd::Random(lidarLens, width);
     for(int i = 0; i < lidarLens; i++){
         matrix_lidar(i,0) = cloud->points[i].x;
@@ -95,47 +96,99 @@ int main()
     int lidarNums = 64;    //激光雷达线束64条
     int everylidarCount = 2190;
 
-    MatrixXd lidar_image   = MatrixXd::Random(lidarNums, everylidarCount );
-    MatrixXd lidar_image_x = MatrixXd::Random(lidarNums, everylidarCount );
-    MatrixXd lidar_image_y = MatrixXd::Random(lidarNums, everylidarCount );
-    MatrixXd lidar_image_z = MatrixXd::Random(lidarNums, everylidarCount );
+    MatrixXd lidar_image   = MatrixXd::Random(lidarNums, everylidarCount);
+    MatrixXd lidar_image_x = MatrixXd::Random(lidarNums, everylidarCount);
+    MatrixXd lidar_image_y = MatrixXd::Random(lidarNums, everylidarCount);
+    MatrixXd lidar_image_z = MatrixXd::Random(lidarNums, everylidarCount);
+    MatrixXd lidar_image_cor = MatrixXd::Random(lidarNums, everylidarCount);
 
     std::vector<std::vector<std::vector<double> > > lidar_image_all(64,vector<vector<double> >(2190,vector<double>(4,0)));
 
-    std::cout << " the size of lidar_image_all: "       <<  lidar_image_all.size()       << endl;
-    std::cout << " the size of lidar_image_all[0]: "    <<  lidar_image_all[0].size()    << endl;
-    std::cout << " the size of lidar_image_all[0][0]: " <<  lidar_image_all[0][0].size() << endl;
+    cout << " the size of lidar_image_all: "       <<  lidar_image_all.size()       << endl;
+    cout << " the size of lidar_image_all[0]: "    <<  lidar_image_all[0].size()    << endl;
+    cout << " the size of lidar_image_all[0][0]: " <<  lidar_image_all[0][0].size() << endl;
+    cout << " lidar_image_all[0][0][0]: "          <<  lidar_image_all[0][0][0]     << endl;
+    cout << " lidar_image_all[63][2189][3]: "      <<  lidar_image_all[63][2189][3] << endl;
 
     // angle_v, angle_h, dangle_h, index = compute_hori_angle(s)
     vector<double> angle_v,  angle_h;
     vector<int> dangle_h, index;
-    compute_hori_angle(matrix_lidar, angle_v, angle_h, dangle_h, index);
+
+    compute_hori_angle(matrix_lidar, angle_v, angle_h, dangle_h, index);    //测试通过
 
 
     int indAnleHold = 0;
-    MatrixXd lidar_image_correspondance = MatrixXd::Random(lidarNums, everylidarCount);
+    int countNot0 = 0;
+    int indAnglePoint = 0;
+    int lastHold = 0;
+
 
     for(int i = 0; i < index.size() - 1; i++){
         int lastAngle = 0;
-        for(int j = index[i], j < index[i+1]; j++){
-            float dis_x = matrix_lidar[i,0];
-            float dis_y = matrix_lidar[i,1];
-            float dis_z = matrix_lidar[j,2];
+        for(int j = index[i]; j < index[i+1]; j++){
+            double dis = sqrt(pow(matrix_lidar(j,0), 2) + pow(matrix_lidar(index[i],1), 2) + pow(matrix_lidar(index[i],2),2) );
+            double dis_x = matrix_lidar(i,0);
+            double dis_y = matrix_lidar(j,1);
+            double dis_z = matrix_lidar(j,2);
 
-            int ind_cur_point = round((180 - angle_h[j]) /0.1728);
+            int ind_cur_point = (int)round((180 - angle_h[j]) /0.1728);
+            if(lidar_image(i, ind_cur_point) != 0){
+                countNot0++;
+            }
 
-            if()
+            if(fabs(lastAngle - angle_h[j]) > 0.1728*1.5){
+                lidar_image(i, ind_cur_point)   = dis;
+                lidar_image_x(i, ind_cur_point) = dis_x;
+                lidar_image_y(i, ind_cur_point) = dis_y;
+                lidar_image_z(i, ind_cur_point) = dis_z;
+                indAnglePoint = j;
+                lastHold = ind_cur_point;
 
+                lidar_image_cor(i, ind_cur_point) = j;
+                /*
+                lidar_image_all[i][ind_cur_point][0] = matrix_lidar(j,0);
+                lidar_image_all[i][ind_cur_point][1] = matrix_lidar(j,1);
+                lidar_image_all[i][ind_cur_point][2] = matrix_lidar(j,2);
+                lidar_image_all[i][ind_cur_point][3] = matrix_lidar(j,3);
+                 */
+            }
 
+            else{
+                lidar_image(i, j - indAnglePoint + lastHold)   = dis;
+                lidar_image_x(i, j - indAnglePoint + lastHold) = dis_x;
+                lidar_image_y(i, j - indAnglePoint + lastHold) = dis_y;
+                lidar_image_z(i, j - indAnglePoint + lastHold) = dis_z;
 
-            if(lidar_image(i,ind_cur_point) != 0){
+                lidar_image_cor(i, j - indAnglePoint + lastHold) = j;
+                /*
+                lidar_image_all[i][j - indAnglePoint + lastHold][0] = matrix_lidar(j,0);
+                lidar_image_all[i][j - indAnglePoint + lastHold][1] = matrix_lidar(j,1);
+                lidar_image_all[i][j - indAnglePoint + lastHold][2] = matrix_lidar(j,2);
+                lidar_image_all[i][j - indAnglePoint + lastHold][3] = matrix_lidar(j,3);
+                 */
 
+            }
+            lastAngle = angle_h[j];
+        }
+    }
+
+    for(int i = 0; i < lidarNums; i++){
+        for(int j = 0; j < everylidarCount; j++){
+            if(lidar_image_x(i,j) > 0.001){
+                lidar_image_x(i,j) = 0;
+            }
+            else{
+                lidar_image_x(i,j) = 1 / lidar_image_x(i,j);
             }
         }
     }
 
 
-
+    for(int i = 0; i < lidarNums; i++){
+        for(int j = 0; j < everylidarCount; j++){
+           lidar_image(i,j) = lidar_image_x(i,j);
+        }
+    }
 
     return 0;
 }
